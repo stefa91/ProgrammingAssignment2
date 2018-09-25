6b9a11efc0946345f8be9093ee27453189e17f34
stefa91 <43033690+stefa91@users.noreply.github.com>
2018-09-25 16:14:34 +0200

    cacheMatrix.R
    edited by stefa91

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Clear workspace
+rm(list = ls(all = TRUE))
 
-## Write a short comment describing this function
+##set path for assignment
 
-makeCacheMatrix <- function(x = matrix()) {
+pathASS2 <- "C:/Users/soliveri/Desktop/PhD/MOOCs/R Programming"
 
-}
+setwd(pathASS2)
 
+##Assignment 2, Part 1
 
-## Write a short comment describing this function
+#Function that creates an object in matrix form that is able to cache its 
+#inverse
+
+#1: set value of matrix
+#2: get value of matric
+#3: set value of inverse
+#4: get value of inverse
+
+makeCacheMatrix <- function(x = matrix()) { 
+        inv <- NULL
+        set <- function(y) { 
+            x <<- y
+            inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inv) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get, 
+             setinv = setinv,
+             getinv = getinv)
+    }
+##Assignment 2, Part 2
+
+#Function that returns the inverse of the matrx created with the previous 
+#function; if inverse was already calculated, it gets the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- inv(data, ...)
+    x$setinv(inv)
+    inv
 }
